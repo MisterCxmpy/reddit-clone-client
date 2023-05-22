e1e07121fbedf577abcd9c9dbe0f8cf195d7f365
MisterCxmpy <cosgunhussein@gmail.com>
2023-05-22 11:42:42 +0100

    add basic styling and layouts to navbar

--- src/layouts/Navbar/index.jsx
+++ src/layouts/Navbar/index.jsx
@@ -1,17 +1,62 @@
-import React from 'react'
-import styles from "./index.module.css"
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import styles from "./index.module.css";
+import { Outlet } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <>
       <div className={styles["navbar"]}>
-        <div className={styles["app-info"]}></div>
-        <div className={styles["user-actions"]}></div>
-        <div className={styles["quick-access"]}></div>
-        <div className={styles["user-profile"]}></div>
+        <AppInfo />
+        <UserActions />
+        <QuickAccess />
+        <UserProfile />
       </div>
       <Outlet />
     </>
-  )
+  );
+}
+
+function AppInfo() {
+  return (
+    <div className={styles["app-info"]}>
+      <div className={styles["logo"]}></div>
+      <span className={styles["title"]}>App Name</span>
+    </div>
+  );
+}
+
+function UserActions() {
+  return (
+    <div className={styles["user-actions"]}>
+      <select className={styles["your-communities"]}>
+        <option value="Your Communities">Your Communities</option>
+      </select>
+      <form className={styles["search-bar"]}>
+        <input type="text" placeholder="Search For Anything" />
+      </form>
+    </div>
+  );
+}
+
+function QuickAccessButton() {
+  return <button className={styles["quick-access-btn"]}></button>;
+}
+
+function QuickAccess() {
+  return (
+    <div className={styles["quick-access"]}>
+      <QuickAccessButton />
+      <QuickAccessButton />
+      <QuickAccessButton />
+      <QuickAccessButton />
+    </div>
+  );
+}
+
+function UserProfile() {
+  return (
+    <div className={styles["user-profile"]}>
+      <div className={styles["profile-picture"]}></div>
+    </div>
+  );
 }
