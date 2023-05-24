281d5e0db5853d3b52c691db9595db88146a1282
MisterCxmpy <cosgunhussein@gmail.com>
2023-05-24 23:16:11 +0100

    saves state of whether dark mode is active or not

--- src/components/UserSideMenu/index.jsx
+++ src/components/UserSideMenu/index.jsx
@@ -1,14 +1,27 @@
-import { forwardRef, useState } from "react";
+import { forwardRef, useEffect, useState } from "react";
 import styles from "./index.module.css";
 import { BiUserCircle } from "react-icons/bi";
 import { HiOutlineEye } from "react-icons/hi";
 
 const UserSideMenu = forwardRef(({ state }, ref) => {
+
+  const [darkMode, setDarkMode] = useState(localStorage.getItem('theme') === 'dark');
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+      localStorage.setItem('theme', 'light');
+    }
+  }, [darkMode]);
+
   return state ? (
     <div ref={ref} className={styles["container"]}>
       <MyInfo />
       <div className={styles["divider"]}></div>
-      <ViewOptions />
+      <ViewOptions darkMode={darkMode} setDarkMode={setDarkMode} />
     </div>
   ) : null;
 });
@@ -27,29 +40,21 @@ function MyInfo() {
   )
 }
 
-function ViewOptions() {
-  const [darkMode, setDarkMode] = useState(true);
-
+function ViewOptions({ darkMode, setDarkMode}) {
+  
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
-
-    if (!darkMode) {
-      document.documentElement.setAttribute('data-theme', 'dark');
-    }
-    else {
-        document.documentElement.setAttribute('data-theme', 'light');
-    } 
   };
 
   return (
-    <div className={styles["section"]}>
-      <span className={styles["title"]}>
-        <div className={styles["title-icon"]}><HiOutlineEye /></div> View Options
+    <div className={styles['section']}>
+      <span className={styles['title']}>
+        <div className={styles['title-icon']}><HiOutlineEye /></div> View Options
       </span>
       <button className={styles['option']} onClick={toggleDarkMode}>
         <span>Dark Mode</span>
         <button className={`${styles.switch} ${darkMode ? styles.on : styles.off}`}></button>
       </button>
     </div>
-  )
+  );
 }
